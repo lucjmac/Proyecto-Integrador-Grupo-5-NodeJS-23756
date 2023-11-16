77bb0add5481d5650a4c5fae76bcf2b89a618c67
Lucas Macias <lucasjmacias@gmail.com>
2023-11-16 16:58:57 -0300

    cambios

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use("/", adminRoutes);
 app.use("/", authRoutes);
 
 //404
-app.use((req, res) => {
-    res.status(404).render("404");
-});
+// app.use((req, res) => {
+//     res.status(404).render("404");
+// });
 
 //Start server
 app.listen(PORT, () => {
