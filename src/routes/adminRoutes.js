04a41ae3af68697b3279a224c53dfac5a4247f44
Lucas Macias <lucasjmacias@gmail.com>
2023-11-16 20:18:44 -0300

    cambios

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -6,10 +6,15 @@ const adminControllers = new adminController();
 
 router
     .get("/admin", adminControllers.adminGet)
+
     .get("/admin/create", adminControllers.adminCreateGet)
+
     .post("/admin/create", adminControllers.adminCreatePost)
+
     .get("/admin/edit/:id", adminControllers.adminEditIdGet)
+
     .put("/admin/edit/:id", adminControllers.adminEditIdPut)
+    
     .delete("/admin/edit/:id", adminControllers.adminEditIdDelete);
 
 export default router;
