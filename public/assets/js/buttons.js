36dff479c86677a7e95820544edae20553d1180f
“Belu <belen.dominguez@mediamonks.com>
2023-12-04 20:39:26 -0300

    fix quantity button not working on cart page

--- public/assets/js/buttons.js
+++ public/assets/js/buttons.js
@@ -19,12 +19,12 @@
 //     const add = document.querySelector(`#${addId}`);
 //     const subtract = document.querySelector(`#${subtractId}`);
 //     const quantity = document.querySelector(`#${quantityId}`);
-  
+
 //     add.addEventListener('click', () => {
 //       console.log('Click en el botón de suma');
 //       quantity.value = Number(quantity.value) + 1;
 //     });
-  
+
 //     subtract.addEventListener('click', () => {
 //       console.log('Click en el botón de resta');
 //       if (quantity.value > 0) {
@@ -33,9 +33,9 @@
 //     });
 //   }
 //     // Fin Función de Yair para vista Item
-    
+
 //     // Para verificar si el archivo actual es 'item.html' o 'cart.html'
-  
+
 //   if (window.location.pathname.includes('item.html')) {
 //     quantityfn('add', 'subtract', 'quantity');
 //   }
@@ -46,27 +46,32 @@
 //   }
 // });
 
-
-
 // Tercera funcion de Luisa, reemplaza document.querySelector por document.getElementById
 
-document.addEventListener('DOMContentLoaded', () => {
-  function setupQuantityControls(addId, subtractId, quantityId) {
-    const add = document.getElementById(addId);
-    const subtract = document.getElementById(subtractId);
-    const quantity = document.getElementById(quantityId);
+document.addEventListener("DOMContentLoaded", () => {
+  function setupQuantityControls() {
+    const quatityWrapper = document.querySelectorAll(
+      ".quantity-action-wrapper"
+    );
 
-    if (add && subtract && quantity) {
-      add.addEventListener('click', () => quantity.value = Number(quantity.value) + 1);
-      subtract.addEventListener('click', () => {
-        if (quantity.value > 0) {
-          quantity.value = Number(quantity.value) - 1;
-        }
-      });
-    }
+    quatityWrapper.forEach((wrapper) => {
+      const add = wrapper.querySelector("#add");
+      const subtract = wrapper.querySelector("#subtract");
+      const quantity = wrapper.querySelector("#quantity");
+
+      if (add && subtract && quantity) {
+        add.addEventListener(
+          "click",
+          () => (quantity.value = Number(quantity.value) + 1)
+        );
+        subtract.addEventListener("click", () => {
+          if (quantity.value > 0) {
+            quantity.value = Number(quantity.value) - 1;
+          }
+        });
+      }
+    });
   }
 
-  setupQuantityControls('add', 'subtract', 'quantity');
-  setupQuantityControls('add1', 'subtract1', 'quantity1');
-  setupQuantityControls('add2', 'subtract2', 'quantity2');
+  setupQuantityControls();
 });
