a6f3503333286815db59c3d41d90afe99a0bdfa0
lfm1383 <54853433+lfm1383@users.noreply.github.com>
2023-12-04 21:12:55 -0500

    iteracion vista shop

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,5 +1,6 @@
 import path from "path";
 import { indexCollections } from "../data/indexCollections.js";
+import { shopCollections } from "../data/shopCollections.js";
 import { sliderItems } from "../data/sliderItems.js";
 
 const viewsPath = path.resolve() + "/src/views";
@@ -10,6 +11,7 @@ export class mainController {
     homeGet(req, res) {
         res.render(path.join(viewsPath, "index.ejs"), {
             indexCollections: indexCollections,
+            shopCollections: shopCollections,
             sliderItems: sliderItems,
         });
     }
