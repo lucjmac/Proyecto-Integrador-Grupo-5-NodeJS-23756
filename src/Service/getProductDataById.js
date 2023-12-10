b76dc1c3078e5357d6792c70552d3bd6d43d71a7
“Belu <belen.dominguez@mediamonks.com>
2023-12-10 17:48:32 -0300

    data connection for item page

--- src/Service/getProductDataById.js
+++ src/Service/getProductDataById.js
@@ -2,3 +2,47 @@
 // export function getProductDataById(productId) {
 //     return productList.find((product) => product.id === productId);
 // }
+
+import conn from "../config/conn.js";
+
+const getProduct = {
+  /**
+   * Consulta por tabla
+   * @param {consulta}
+   */
+  consulta: async (id) => {
+    try {
+      console.log("Consulta a la tabla " + id);
+      const query = `SELECT *  FROM product WHERE product_id  = ${id}`;
+      const consulta = await conn.query(query);
+
+      return consulta;
+    } catch (error) {
+      console.log("Error al realizar conexion con BBDD: " + error);
+    }
+  },
+  consultaCategory: async (id) => {
+    try {
+      console.log("Consulta a la tabla " + id);
+      const query = `SELECT *  FROM category WHERE id  = ${id}`;
+      const consulta = await conn.query(query);
+
+      return consulta;
+    } catch (error) {
+      console.log("Error al realizar conexion con BBDD: " + error);
+    }
+  },
+  consultaLicence: async (id) => {
+    try {
+      console.log("Consulta a la tabla " + id);
+      const query = `SELECT *  FROM licence WHERE id  = ${id}`;
+      const consulta = await conn.query(query);
+
+      return consulta;
+    } catch (error) {
+      console.log("Error al realizar conexion con BBDD: " + error);
+    }
+  },
+};
+
+export default getProduct;
--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -3,32 +3,45 @@ import { sliderItems } from "../data/sliderItems.js";
 import { shopCollections } from "../data/shopCollections.js";
 import { cartItems } from "../data/cartItems.js";
 
+import getProduct from "../Service/getProductDataById.js";
+
 const viewsPath = path.resolve() + "/src/views/shop";
 
 export class shopController {
-    constructor() {}
+  constructor() {}
 
-    shopGet(req, res) {
-        res.render(path.join(viewsPath, "shop.ejs"), {
-            shopCollections: shopCollections,
-        });
-    }
+  shopGet(req, res) {
+    res.render(path.join(viewsPath, "shop.ejs"), {
+      shopCollections: shopCollections,
+    });
+  }
 
-    itemIdGet(req, res) {
-        res.render(path.join(viewsPath, "item.ejs"), {
-            sliderItems: sliderItems,
-        });
-    }
+  async itemIdGet(req, res) {
+    const id = req.params.id;
+    const product = await getProduct.consulta(id);
+    const { category_id, licence_id } = product[0][0];
+    const category = await getProduct.consultaCategory(category_id);
+    const licence = await getProduct.consultaLicence(licence_id);
+    console.log("qqq", product[0][0]);
+    console.log("category", category);
+    console.log("licence", licence);
+    res.render(path.join(viewsPath, "item.ejs"), {
+      sliderItems: sliderItems,
+      product: product[0][0],
+      category,
+      licence: licence[0][0],
+    });
+  }
 
-    shopCartGet(req, res) {
-        res.render(path.join(viewsPath, "cart.ejs"), { cartItems });
-    }
+  shopCartGet(req, res) {
+    res.render(path.join(viewsPath, "cart.ejs"), { cartItems });
+  }
 
-    itemIdAddPost(req, res) {
-        res.send("Route for add the current item to the shop cart ");
-    }
+  itemIdAddPost(req, res) {
+    res.send("Route for add the current item to the shop cart ");
+  }
 
-    shopCartPost(req, res) {
-        res.send("Route for go to checkout page");
-    }
+  shopCartPost(req, res) {
+    res.send("Route for go to checkout page");
+  }
 }
