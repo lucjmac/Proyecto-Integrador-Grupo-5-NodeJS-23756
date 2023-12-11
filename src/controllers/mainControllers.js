308b56a07250a58c6e9fc1e65faea6315233eaf5
Lucas Macias <lucasjmacias@gmail.com>
2023-12-11 18:25:44 -0300

    cambios, continua sin funcionar

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -13,35 +13,38 @@ export class mainController {
             const indexCollections = await conn.query(
                 "SELECT id, licence_name, licence_description, licence_alt FROM licence"
             );
+            console.log("indexCollections:", indexCollections); // Imprimir los valores de indexCollections
+    
             const sliderItems = await conn.query(
                 "SELECT product_id, product_name, product_description, image_Front, image_Back, licence_id, price, dues FROM product"
             );
-
+            console.log("sliderItems:", sliderItems); // Imprimir los valores de sliderItems
+    
             const itemsWithModifiedData = sliderItems.map((item) => {
                 const licence = indexCollections.find(
                     (collection) => collection.id === item.licence_id
                 );
                 const modifiedItem = { ...item };
-
+    
                 modifiedItem.link = `/item/${item.product_name}`;
                 modifiedItem.licence_id = licence ? licence.licence_name : "";
                 modifiedItem.duesText =
                     item.dues === 1
                         ? "cuota sin interés"
                         : `${item.dues} cuotas sin interés`;
-
+    
                 return modifiedItem;
             });
-
+    
+            console.log("itemsWithModifiedData:", itemsWithModifiedData); // Imprimir los valores de itemsWithModifiedData
+    
             res.render(path.join(viewsPath, "index.ejs"), {
                 indexCollections: indexCollections,
                 sliderItems: itemsWithModifiedData,
             });
         } catch (error) {
             console.error(error);
-            res.status(500).send(
-                "Error al obtener los datos de la base de datos"
-            );
+            res.status(500).send("Error al obtener los datos de la base de datos");
         }
     }
 
