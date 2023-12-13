e9a00353778d096dffa9cba97ebb145eeeec3216
Lucas Macias <lucasjmacias@gmail.com>
2023-12-13 12:28:23 -0300

    input del admin funcionando completamente desde DB

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,31 +1,45 @@
 import path from "path";
-import { productList } from "../data/productList.js";
-import { getProductByIdAdminToEdit } from "../service/getProductByIdAdminToEdit.js";
+import { conn } from "../config/conn.js";
+// import { productList } from "../data/productList.js";
+// import { getProductByIdAdminToEdit } from "../service/getProductByIdAdminToEdit.js";
 
 const viewsPath = path.resolve() + "/src/views/admin";
 
 export class adminController {
     constructor() {}
 
-    adminGet(req, res) {
+    async adminGet(req, res) {
         const searchInput = req.query.searchInput;
     
-        const filteredProductList = productList.filter(
-            (product) =>
-                !searchInput || 
-                searchInput.trim() === "" || 
-                product.name.includes(searchInput) 
-        );
+        try {
+            const [productResults] = await conn.query("SELECT * FROM product");
+            const productList = productResults;
     
-        req.filteredProductList = filteredProductList;
-        req.searchInput = searchInput;
-        req.noResults = filteredProductList.length === 0;
+            for (const product of productList) {
+                const [licenceResults] = await conn.query("SELECT licence_name FROM licence WHERE id = ?", [product.licence_id]);
+                const licence = licenceResults[0];
+                product.licence_name = licence ? licence.licence_name : "Unknown Licence";
+            }
     
-        res.render(path.join(viewsPath, "admin.ejs"), {
-            productList: filteredProductList,
-            searchInput: searchInput,
-            noResults: req.noResults,
-        });
+            const filteredProductList = productList.filter(
+                (product) =>
+                    !searchInput ||
+                    searchInput.trim() === "" ||
+                    product.name.includes(searchInput)
+            );
+    
+            req.filteredProductList = filteredProductList;
+            req.searchInput = searchInput;
+            req.noResults = filteredProductList.length === 0;
+    
+            res.render(path.join(viewsPath, "admin.ejs"), {
+                productList: filteredProductList,
+                searchInput: searchInput,
+                noResults: req.noResults,
+            });
+        } catch (error) {
+            console.error("Error executing queries:", error);
+        }
     }
 
     adminCreateGet(req, res) {
@@ -36,19 +50,19 @@ export class adminController {
         res.send("Route for create with POST in Admin View");
     }
 
-    adminEditIdGet(req, res) {
-        const productId = req.params.id;
-        const product = getProductByIdAdminToEdit(productId);
+    // adminEditIdGet(req, res) {
+    //     const productId = req.params.id;
+    //     const product = getProductByIdAdminToEdit(productId);
 
-        res.render(path.join(viewsPath, "edit.ejs"), {
-            productList,
-            productId,
-            product,
-            code: product ? product.code : "",
-            name: product ? product.name : "",
-            collection: product ? product.collection : "",
-        });
-    }
+    //     res.render(path.join(viewsPath, "edit.ejs"), {
+    //         productList,
+    //         productId,
+    //         product,
+    //         code: product ? product.code : "",
+    //         name: product ? product.name : "",
+    //         collection: product ? product.collection : "",
+    //     });
+    // }
 
     adminEditIdPut(req, res) {
         res.send("Route for edit/:id put");
