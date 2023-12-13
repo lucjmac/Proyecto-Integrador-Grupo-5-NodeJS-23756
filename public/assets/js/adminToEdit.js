8e9e60e075a0bb07535ea07b567ff1a6a27f2292
Lucas Macias <lucasjmacias@gmail.com>
2023-12-13 11:24:38 -0300

    elimine comentarios para trabajar

--- public/assets/js/adminToEdit.js
+++ public/assets/js/adminToEdit.js
@@ -1,29 +1,19 @@
 if (typeof product !== "undefined" && typeof product.id !== "undefined") {
-    // si estan definidos producto y su código 
-    
-    // obtiene el código del producto
     var productId = product.id;
-    
-    // obtiene el elemento select con el id "code"
+
     var selectElement = document.getElementById("code");
-    
-    // recorre las opciones del select
+
     for (var i = 0; i < selectElement.options.length; i++) {
         var optionElement = selectElement.options[i];
-        
-        // si el valor de la opción coincide con el código del producto
+
         if (optionElement.value === productId) {
-            // marca la opción como seleccionada
             optionElement.selected = true;
-            
-            // busca el producto en la lista de productos
+
             var selectedProduct = productList.find(function (product) {
                 return product.id === productId;
             });
-            
-            // si se encuentra el producto correspondiente
+
             if (typeof selectedProduct !== "undefined") {
-                // obtiene los elementos correspondientes a los campos del formulario
                 var codeElement = document.getElementById("code");
                 var categoryElement = document.getElementById("category");
                 var collectionElement = document.getElementById("collection");
@@ -35,8 +25,7 @@ if (typeof product !== "undefined" && typeof product.id !== "undefined") {
                 var cuotasElement = document.getElementById("cuotas");
                 var imgfrontElement = document.getElementById("imgfront");
                 var imgbackElement = document.getElementById("imgback");
-                
-                // actualiza los valores de los campos del formulario con la información del producto en edit.ejs
+
                 codeElement.value = selectedProduct.code;
                 collectionElement.value = selectedProduct.collection;
                 nameElement.value = selectedProduct.name;
@@ -44,6 +33,5 @@ if (typeof product !== "undefined" && typeof product.id !== "undefined") {
         }
     }
 } else {
-    // pero si el product.code no está definido
     console.error("product.id no está definido o no tiene un valor válido");
 }
