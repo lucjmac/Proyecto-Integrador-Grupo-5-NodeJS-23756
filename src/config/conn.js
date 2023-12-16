5a8d8782c78d8544a29c5f333ac557f5f78c5bfa
Lucas Macias <lucasjmacias@gmail.com>
2023-12-16 10:58:15 -0300

    Reajustes DB conection

--- src/config/conn.js
+++ src/config/conn.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node_23576_grupo_5',
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
