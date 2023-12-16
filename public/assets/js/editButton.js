7a9bda02f3ef53e809b59c8b3477255d1e7158c2
“Belu <belen.dominguez@mediamonks.com>
2023-12-16 20:46:18 -0300

    fix edit enpoint. fix select on edit page

--- public/assets/js/editButton.js
+++ public/assets/js/editButton.js
@@ -1,51 +1,63 @@
-const button = document.getElementById('modificar');
+const HOST = window.location.origin;
+const button = document.getElementById("modificar");
 
-    button.addEventListener('click', () => {
-        const productId = button.dataset.product_id;
+button.addEventListener("click", () => {
+  const productId = button.dataset.productId;
+  console.log("w", productId, button.dataset);
+  const newProductCategory = document.querySelector(".category_name").value;
+  const newProductlicence = document.querySelector(".licence_name").value;
+  const newProductName = document.querySelector(".product_name").value;
+  const newProductDescription = document.querySelector(
+    ".product_description"
+  ).value;
+  const newProductSku = document.querySelector(".sku").value;
 
-        const newProductCategory = document.querySelector('.category_name').value;
-        const newProductlicence = document.querySelector('.licence_name').value;
-        const newProductName = document.querySelector('.product_name').value; 
-        const newProductDescription = document.querySelector('.product_description').value;
-        const newProductSku = document.querySelector('.sku').value;
-        const newProductPrice = document.querySelector('.price').value;
-        const newProductStock = document.querySelector('.stock').value;
-        const newProductDiscount = document.querySelector('.discount').value;
-        const newProductDues = document.querySelector('.dues').value;
-        const newProductImgFront = document.querySelector('.image_Front').value;
-        const newProductImgBack = document.querySelector('.image_Back').value;
+  let newProductPrice = document.querySelector(".price").value;
+  newProductPrice = Number(newProductPrice.split("$")[1].trim());
 
-        const updatedProduct = {
-            category_name: newProductCategory,
-            licence_name: newProductlicence,
-            product_name: newProductName,
-            product_description: newProductDescription,
-            sku: newProductSku,
-            price: newProductPrice,
-            stock: newProductStock,
-            discount: newProductDiscount,
-            dues: newProductDues,
-            image_Front: newProductImgFront,
-            image_Back: newProductImgBack
-        };
+  const newProductStock = document.querySelector(".stock").value;
+  const newProductDiscount = document.querySelector(".discount").value;
+  const newProductDues = document.querySelector(".dues").value;
+  const imgFront = document.querySelector(".image_Front");
+  const newProductImgFront = imgFront.value || imgFront.dataset.value;
+  const imgBack = document.querySelector(".image_Back");
+  const newProductImgBack = imgBack.value || imgBack.dataset.value;
 
-        fetch(`/admin/edit/${productId}`, {
-            method: 'PUT', 
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(updatedProduct)
-        })
-        .then(response => {
-            if (response.ok) {
-                console.log('Producto actualizado exitosamente');
-                
-                window.location.href = '/admin';
-            } else {
-                console.error('Error al actualizar el producto');
-            }
-        })
-        .catch(error => {
-            console.error('Error en la solicitud PUT:', error);
-        });
+  const updatedProduct = {
+    category_name: newProductCategory,
+    licence_name: newProductlicence,
+    product_name: newProductName,
+    product_description: newProductDescription,
+    sku: newProductSku,
+    price: newProductPrice,
+    stock: newProductStock,
+    discount: newProductDiscount,
+    dues: newProductDues,
+    image_Front: newProductImgFront,
+    image_Back: newProductImgBack,
+  };
+
+  fetch(`${HOST}/admin/edit/${productId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedProduct),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Producto actualizado exitosamente");
+
+        const backToAdmin = confirm(
+          "Producto modificado exitosamente. Desea volver a la lista de productos?"
+        );
+
+        if (backToAdmin) window.location.href = "/admin";
+      } else {
+        console.error("Error al actualizar el producto");
+      }
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud PUT:", error);
     });
+});
